3c2db3971fced19f8f2eb0cf3b8cd5fdc999434c
chaosua <544218+chaosua@users.noreply.github.com>
2024-12-06 23:03:09 +0200

    release Launcher.exe 1.0.0.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Freedom WoW UA Launcher")]
-[assembly: AssemblyCopyright("Copyright © Luth 2018, © Lighto 2024")]
+[assembly: AssemblyCopyright("Copyright © Luth 2018, © Lighto 2024, © nextepleez 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
